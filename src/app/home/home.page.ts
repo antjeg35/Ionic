7b451e7aa5e68298ed075f95874cac07adf0fae8
Antoine JEGOU <ajegou@groupe-beaumanoir.fr>
2020-06-30 12:39:50 +0200

    Module4TP

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  pseudo= '';
+  level= '';
+  save= false;
+  error= '';
+  blocLogin= true;
+  blocFirstQuestion = false;
+  buttonNextQuestion = false;
 
   constructor() {}
+  
+
+  start(){
+    this.error = "";
+    if(this.pseudo.length < 3 || this.level === ""){
+      return this.error = 'Veuillez rentrer un pseudo ou une difficulté correcte'
+    } else{
+      this.blocFirstQuestion = true;
+      this.blocLogin = false;
+      return;
+    }
+  }
+
+  firstQuestion($event){
+    if(typeof $event != 'undefined'){
+      return this.buttonNextQuestion = true;
+    }
+  }
+
+
 
 }
