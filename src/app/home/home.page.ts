f7e3747012feeab3ecc07976f6ec49b5924845aa
Antoine JEGOU <ajegou@groupe-beaumanoir.fr>
2020-07-01 14:25:56 +0200

    Module6TP

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ToastController } from '@ionic/angular';
+import { OpenTrivialService } from '../providers/OpenTrivialService.provider';
 
 @Component({
   selector: 'app-home',
@@ -13,10 +14,15 @@ export class HomePage {
   error= '';
   blocFirstQuestion = false;
   buttonNextQuestion = false;
+  questions=[];
+  currentQuestion;
+  idQuestion=0;
+  points = 0; 
+  endGame = false;
 
-  constructor(public toastController: ToastController) {}
+  constructor(public toastController: ToastController, private openTrivialService: OpenTrivialService) {}
   
-  async invaliPseudoToast() {
+  async invalidePseudoToast() {
     const toast = await this.toastController.create({
       message: 'Veuillez rentrer un pseudo ou une difficulté correcte.',
       duration: 4000,
@@ -25,19 +31,46 @@ export class HomePage {
     toast.present();
   }
 
-
   start(){
     this.error = "";
     if(this.pseudo.length < 3 || this.level === ""){
-      return this.invaliPseudoToast();
+      return this.invalidePseudoToast();
     } else{
+      this.launchQuizz();
       return this.blocFirstQuestion = true;
     }
   }
 
-  firstQuestion($event){
-    if(typeof $event != 'undefined'){
-      return this.buttonNextQuestion = true;
+  private async launchQuizz(){
+    try{
+      this.questions= await this.openTrivialService.getQuestions(1, this.level);
+      this.handleQuestion();
+    }catch(err){
+      this.error= "Impossible de récupèrer les questions !"
     }
   }
+
+  handleQuestion(){
+    this.currentQuestion = this.questions[this.idQuestion];
+    this.currentQuestion["incorrect_answers"].push(this.currentQuestion["correct_answer"]);
+  }
+
+  reponse(reponse: string) { 
+    this.buttonNextQuestion = true;
+    if (reponse == this.currentQuestion.correct_answer) { 
+      this.points++; 
+    } 
+    if (this.idQuestion >= this.questions.length - 1) 
+    { this.endGame = true; } 
+  }
+
+  nextQuestion() {
+    if (this.idQuestion < this.questions.length - 1) { 
+      this.idQuestion++; 
+      this.buttonNextQuestion = false; 
+      this.launchQuizz();
+    } 
+  }
+
+  retour() { this.blocFirstQuestion = false; }
 }
